04f50e76ef5454dfcd716f0d633195e09a73f373
daruken <dj.hongten@gmail.com>
2024-04-07 21:54:36 +0900

    🎉 chore: 테스트 프로젝트 시작

---
+++ rabbit/src/main/kotlin/org/dpluskia/rabbit/RabbitApplication.kt
@@ -0,0 +1,11 @@
+package org.dpluskia.rabbit
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RabbitApplication
+
+fun main(args: Array<String>) {
+    runApplication<RabbitApplication>(*args)
+}
---
+++ src/test/kotlin/org/dpluskia/kingen/KingenApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.dpluskia.kingen
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KingenApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
